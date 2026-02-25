fa96a715312ad4086abd8ea091e9a0712c731a8f
Luke Carrier <luke@carrier.family>
2026-02-25 00:02:53 +0000

    trying to fix desktop build

--- nix/desktop.nix
+++ nix/desktop.nix
@@ -33,7 +33,14 @@ rustPlatform.buildRustPackage (finalAttrs: {
     ;
 
   cargoRoot = "packages/desktop/src-tauri";
-  cargoLock.lockFile = ../packages/desktop/src-tauri/Cargo.lock;
+  cargoLock = {
+    lockFile = ../packages/desktop/src-tauri/Cargo.lock;
+    outputHashes = {
+      "specta-2.0.0-rc.22" = "sha256-YsyOAnXELLKzhNlJ35dHA6KGbs0wTAX/nlQoW8wWyJQ=";
+      "tauri-2.9.5" = "sha256-dv5E/+A49ZBvnUQUkCGGJ21iHrVvrhHKNcpUctivJ8M=";
+      "tauri-specta-2.0.0-rc.21" = "sha256-n2VJ+B1nVrh6zQoZyfMoctqP+Csh7eVHRXwUQuiQjaQ=";
+    };
+  };
   buildAndTestSubdir = finalAttrs.cargoRoot;
 
   nativeBuildInputs = [
